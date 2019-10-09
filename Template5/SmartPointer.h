c1f79d7e0a0987975de7cb4fdb217a1fce5e0718
Gaspar <gaspar.herman95@gmail.com>
2019-10-09 20:49:01 +0200

    base aumentada

--- Template5/SmartPointer.h
+++ Template5/SmartPointer.h
@@ -1,29 +1,62 @@
 #pragma once
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 
 template <class T>
 class SmartPointer
 {
 public:
-	SmartPointer() : m_originalPointer(nullptr) {};
+	SmartPointer() : m_originalPointer(nullptr)  {};
 	SmartPointer<T>(T* &pointerToCast) {
+		bool isDeleteNeeded = false;
+		if (m_originalPointer != nullptr){
+			isDeleteNeeded = true;
+		}
 
-		m_originalPointer = pointerToCast;
-		pointerToCast = nullptr;
+		m_originalPointer = new T;
+		*m_originalPointer = *pointerToCast;
+
+		if(isDeleteNeeded){
+			delete pointerToCast;
+		}
 
 	}
 
 	SmartPointer<T> operator=(T* &pointerToCast){
+
+		bool isDeleteNeeded = false;
 		if (m_originalPointer != nullptr){
+			isDeleteNeeded = true;
+		}
+
+		m_originalPointer = new T;
+		*m_originalPointer = *pointerToCast;
+
+		if(isDeleteNeeded){
 			delete pointerToCast;
 		}
 
-		m_originalPointer = pointerToCast;
-		pointerToCast = nullptr;
-
 		return *this;
 	}
+
+	SmartPointer<T> operator =(SmartPointer<T> spToAsign){
+		m_originalPointer = new T;
+		*m_originalPointer = spToAsign.*m_originalPointer;
+
+		spToAsign.m_originalPointer = nullptr;
+
+	}
+
+	T &operator *() {
+		return *m_originalPointer;
+	}
+
+//	void* operator new(size_t const size){
+//		void *voidPointer =  malloc(size);
+//		return voidPointer;
+//	}
+
 
 	bool getInfo() { return m_originalPointer == nullptr ;}
 private:
