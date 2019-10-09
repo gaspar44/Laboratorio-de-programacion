8af08d639b1727688b878126efd38a83efe0578a
Gaspar <gaspar.herman95@gmail.com>
2019-10-09 18:43:43 +0200

    Inicio de SmartPointer

--- Template5/SmartPointer.h
+++ Template5/SmartPointer.h
@@ -6,7 +6,27 @@ template <class T>
 class SmartPointer
 {
 public:
-private:
+	SmartPointer() : m_originalPointer(nullptr) {};
+	SmartPointer<T>(T* &pointerToCast) {
 
+		m_originalPointer = pointerToCast;
+		pointerToCast = nullptr;
+
+	}
+
+	SmartPointer<T> operator=(T* &pointerToCast){
+		if (m_originalPointer != nullptr){
+			delete pointerToCast;
+		}
+
+		m_originalPointer = pointerToCast;
+		pointerToCast = nullptr;
+
+		return *this;
+	}
+
+	bool getInfo() { return m_originalPointer == nullptr ;}
+private:
+	T* m_originalPointer;
 };
 
