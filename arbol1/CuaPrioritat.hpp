a1cc94be52fdde4eea81777ce908f08b409a8af5
Gaspar <gaspar.herman95@gmail.com>
2019-11-28 18:26:52 +0100

    Primera compilación del ejercicio de árbol

---
+++ arbol1/CuaPrioritat.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include "Heap.hpp"
+
+template <class T>
+class CuaPrioritat
+{
+public:
+	CuaPrioritat() {}
+	~CuaPrioritat() {}
+	CuaPrioritat(const CuaPrioritat<T>& cua) {
+		m_heap = cua.m_heap;
+	}
+
+	CuaPrioritat& operator=(const CuaPrioritat<T>& cua){
+		m_heap = cua.m_heap;
+		return *this;
+	}
+
+	bool esBuida() const {
+		return m_heap.getMida() == -1;
+	}
+
+	int getNElements() const{
+		return m_heap.getMida();
+	}
+
+	T& top(){
+		return m_heap.getMinim();
+	}
+
+	void push(const T& element) {
+		m_heap.insereix(element);
+	}
+
+	void pop(){
+		m_heap.esborraMinim();
+	}
+
+	void remove(const T& element){
+		m_heap.esborra(element);
+	}
+
+private:
+	Heap<T> m_heap;
+};
---
+++ arbol1/Tasca.cpp
@@ -0,0 +1,28 @@
+#include "Tasca.h"
+
+bool Tasca::operator<(const Tasca& tasca)
+{
+	bool menor = false;
+	if (m_prioritat < tasca.m_prioritat)
+		menor = true;
+	else
+		if ((m_prioritat == tasca.m_prioritat) && (m_data < tasca.m_data))
+			menor = true;
+	return menor;
+}
+
+bool Tasca::operator >(const Tasca& tasca){
+
+	if (m_prioritat < tasca.m_prioritat)
+		return true;
+	else
+		if ((m_prioritat == tasca.m_prioritat) && (m_data < tasca.m_data))
+			return true;
+
+	return false;
+}
+
+bool Tasca::operator==(const Tasca& tasca)
+{
+	return (m_codi == tasca.m_codi);
+}
